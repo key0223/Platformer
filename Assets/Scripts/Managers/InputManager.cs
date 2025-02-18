6b139dd6e2b12c7bbf4e44de90883165c888f558
addkey <addkey0223@gmail.com>
2025-02-18 11:42:55 +0900

    [기능] InputManager 추가 및 InventoryUI 토글 기능 추가

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : SingletonMonobehaviour<InputManager>
+{
+    public event Action<bool> OnUIStateChanged;
+    public event Action OnToggleInventory;
+
+    bool _isUIOn = false;
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            _isUIOn = !_isUIOn;
+            UIStateChanged(_isUIOn);
+            ToggleInventory();
+        }
+    }
+
+    public void UIStateChanged(bool state)
+    {
+        OnUIStateChanged?.Invoke(_isUIOn);
+    }
+    public void ToggleInventory()
+    {
+        OnToggleInventory?.Invoke();
+    }
+}
