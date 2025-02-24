90936a0fa7e6caf94e36a782a10afdbb53d1948e
addkey <addkey0223@gmail.com>
2025-02-24 18:30:59 +0900

    [개선] Stat -> MonsterStat 클래스로 분리

--- Assets/Scripts/Contents/Stat.cs
+++ Assets/Scripts/Contents/Stat.cs
@@ -10,18 +10,16 @@ public class Stat : MonoBehaviour
     [SerializeField] protected float _maxHp = 100;
     [SerializeField] protected float _attack = 10;
     [SerializeField] protected float _defense = 0;
-    [SerializeField] protected float _needExpToNextLevel = 100;
     [SerializeField] protected float _currentHp;
     protected float _currentAttack;
 
     public float MaxHp { get { return _maxHp; } }
     public float Attack { get { return _attack; } }
     public float Defense { get { return _defense; } }
-    public float NeedExpToNextLevel { get { return _needExpToNextLevel; } }
     public float CurrentHp { get { return _currentHp; } set { _currentHp = value; } }
     public float CurrentAttack { get { return _currentAttack; } set { _currentAttack = value; } }
 
-    protected void Start()
+    protected virtual void Start()
     {
         _currentHp = _maxHp;
         _currentAttack = _attack;
