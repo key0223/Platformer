14a1f601759b2474c75fdc158401eea08cc1c012
addkey <addkey0223@gmail.com>
2025-03-10 17:21:51 +0900

    [UI] InformationPanel 관리 클래스 추가

---
+++ Assets/Scripts/Contents/UI/SpellInfoPanel.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpellInfoPanel : InformationPanelBase
+{
+
+    [SerializeField] TextMeshProUGUI _loreText;
+    [SerializeField] Image _descriptopnImage;
+    [SerializeField] TextMeshProUGUI _descriptopnText;
+    [SerializeField] TextMeshProUGUI _keyDescriptionText;
+    [SerializeField] TextMeshProUGUI _keyText;
+    [SerializeField] TextMeshProUGUI __descriptopnText2;
+    protected override void Init()
+    {
+
+    }
+}
