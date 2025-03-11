f3898ed02a4eb49bcebfd1c843372ad8c216b4e9
addkey <addkey0223@gmail.com>
2025-03-11 11:26:52 +0900

    [기능] Interactable Object 추가
    상호작용 가능한 LoreObject 클래스 추가

---
+++ Assets/Scripts/Contents/Objects/LoreObject.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Define;
+
+public class LoreObject : MonoBehaviour, IInteractable
+{
+    PopupPanel _popup;
+
+    [SerializeField] InfoPanelType _infoPanelType;
+    [SerializeField] int _loreStrId;
+    [SerializeField] int _spellId;
+
+    [SerializeField] bool _isPlayerNear = false;
+
+
+    void Start()
+    {
+        _popup = UIManager.Instance.PopupPanel;
+    }
+    void Update()
+    {
+        if (_isPlayerNear && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Interact();
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag(TAG_PLAYER))
+        {
+            _isPlayerNear = true;
+
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag(TAG_PLAYER))
+        {
+            _isPlayerNear = false;
+
+        }
+    }
+    public void Interact()
+    {
+        // TODO : Information Panel Popup
+
+        _popup.InfoPanel.PopupUI();
+    }
+
+   
+}
