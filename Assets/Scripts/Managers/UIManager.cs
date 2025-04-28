603ed39db6e04234ded977e027c0cccee7128f02
addkey <addkey0223@gmail.com>
2025-04-28 12:10:46 +0900

    [UI] Interaction 가능 UI 추가

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -7,12 +7,15 @@ public class UIManager : SingletonMonobehaviour<UIManager>
     [SerializeField] HUDPanel _hudUIPanel;
     [SerializeField] PopupPanel _popupUIPanel;
     [SerializeField] DialoguePanel _dialoguePanel;
+    [SerializeField] InteractionStartUI _interactionStartUI;
 
     public HUDPanel HUDPanel { get { return _hudUIPanel; } }
     public PopupPanel PopupPanel { get { return _popupUIPanel; } }
 
     public DialoguePanel DialoguePanel {  get { return _dialoguePanel; } }
 
+    // World space UI
+    public InteractionStartUI InteractionStartUI { get { return _interactionStartUI; } }
     protected override void Awake()
     {
         base.Awake();
