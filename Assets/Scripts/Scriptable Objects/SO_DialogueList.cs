c474808e66d440622ecc5598ca587becc54bbd80
addkey <addkey0223@gmail.com>
2025-04-30 11:27:26 +0900

    [데이터] Elderbug 데이터 추가

--- Assets/Scripts/Scriptable Objects/SO_DialogueList.cs
+++ Assets/Scripts/Scriptable Objects/SO_DialogueList.cs
@@ -8,6 +8,10 @@ public class SO_DialogueList : ScriptableObject, ILoader<string, DialogueNode>
     [SerializeField]
     public List<DialogueNode> iseldaDialogues;
 
+    [SerializeField]
+    public List<DialogueNode> elderbugDialogues;
+
+
     public Dictionary<string, DialogueNode> MakeDict()
     {
         Dictionary<string,DialogueNode> dict = new Dictionary<string, DialogueNode>();
@@ -17,6 +21,10 @@ public class SO_DialogueList : ScriptableObject, ILoader<string, DialogueNode>
             dict.Add(dialogue.nodeId, dialogue);
         }
 
+        foreach(DialogueNode dialogue in elderbugDialogues)
+        {
+            dict.Add(dialogue.nodeId,dialogue);
+        }
         return dict;
     }
 }
